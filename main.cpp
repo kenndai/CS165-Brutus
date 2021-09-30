cf0df97e8a12ddc00749584ab4a03d39000526b3
kenndai <heyitskennydai@gmail.com>
2021-09-29 21:48:51 -0700

    added one final for loop, code generates all combinations

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 const string alphabet[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-int numCombinations = 0;
 
 void generateString(int);
 
@@ -16,18 +15,16 @@ int main() {
     return 0;
 }
 
-
 void generateString(int stringLength) {
     string generatedString;
     for (int i = 0; i < stringLength; i++)
         for (int j = 0; j < stringLength; j++)
             for (int k = 0; k < stringLength; k++)
                 for (int x = 0; x < stringLength; x++)
-                    for (int y = 0; y < stringLength; y++) {
-                        generatedString = alphabet[i] + alphabet[j] + alphabet[k] + alphabet[x] + alphabet[y];
-                        // if (hash(generatedString) == hashedPassword) return generatedString
-                        cout << generatedString << endl;
-                        numCombinations++;
-                    }
-    cout << "number of combinations: " << numCombinations << endl;
+                    for (int y = 0; y < stringLength; y++)
+                        for (int z = 0; z < stringLength; z++) {
+                            generatedString = alphabet[i] + alphabet[j] + alphabet[k] + alphabet[x] + alphabet[y] + alphabet[z];
+                            // if (hash(generatedString) == hashedPassword) return generatedString
+                            cout << generatedString << endl;
+                        }
 }
