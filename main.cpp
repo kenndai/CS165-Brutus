f24ba5b5331a9e26e1a4a298101343929522ecd9
kenndai <heyitskennydai@gmail.com>
2021-10-02 02:58:05 -0700

    Reformatted code

--- main.cpp
+++ main.cpp
@@ -2,41 +2,21 @@
 #include <string>
 #include "md5.h"
 #include <chrono>
-//TODO #include md5 library
 
 using namespace std;
 
-void generateString(int);
-string sixBytes(string);
-string MD5Crypt(string);
-
-// To check a password, you’d read the hash from /etc/shadow, extract the salt, run the algorithm (crypt()?)
-// on this salt and the candidate password, and then see if the resulting hash matches what you have.
-
-//TODO 1. Alternate Sum: md5(salt + pass + salt)
-//TODO 2.
+string altSixBytes(string);
+string MD5_Crypt(string);
+string crack();
 
 const string salt = "4fTgjp6q";
 const string magic = "$1$";
-const string hash = "CxWQeLPwIdqToYNk5yDoS.";
+const string hashedPassword = "CxWQeLPwIdqToYNk5yDoS.";
 const string alphabet[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
 int main() {
+    cout << "crack() goes here" << endl;
     return 0;
-}
-
-void generateString() {
-    string generatedString;
-    for (int i = 0; i < 26; i++)
-        for (int j = 0; j < 26; j++)
-            for (int k = 0; k < 26; k++)
-                for (int x = 0; x < 26; x++)
-                    for (int y = 0; y < 26; y++)
-                        for (int z = 0; z < 26; z++) {
-                            generatedString = alphabet[i] + alphabet[j] + alphabet[k] + alphabet[x] + alphabet[y] + alphabet[z];
-                            // if (MD5Crypt(generatedString) == hashedPassword) return generatedString
-                            cout << generatedString << endl;
-                        }
 }
 
 string altSixBytes(string password) {
@@ -44,7 +24,7 @@ string altSixBytes(string password) {
     return md5(passSaltPass.substr(0, 6));
 }
 
-string MD5Crypt(string password) {
+string MD5_Crypt(string password) {
     string concat = password + magic + salt + altSixBytes(password) + password[0] + "\0" + "\0";
     string zeroSum = md5(concat);
     for (int i = 0; i < 1000; i++) {
@@ -57,5 +37,19 @@ string MD5Crypt(string password) {
         if (i % 2 != 0) intermediate += zeroSum;
         zeroSum = md5(intermediate);
     }
+    //TODO Replacement
     return "hi";
+}
+
+string crack() {
+    string generatedString;
+    for (int i = 0; i < 26; i++)
+        for (int j = 0; j < 26; j++)
+            for (int k = 0; k < 26; k++)
+                for (int x = 0; x < 26; x++)
+                    for (int y = 0; y < 26; y++)
+                        for (int z = 0; z < 26; z++) {
+                            generatedString = alphabet[i] + alphabet[j] + alphabet[k] + alphabet[x] + alphabet[y] + alphabet[z];
+                            if (MD5_Crypt(generatedString) == hashedPassword) return generatedString;
+                        }
 }
